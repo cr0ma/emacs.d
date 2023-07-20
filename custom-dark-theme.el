e9383844b5fd3196899f502edccec298866fe387
cr0ma <cr0ma@tracciabi.li>
2023-07-20 16:08:46 +0200

    changed some things

--- custom-dark-theme.el
+++ custom-dark-theme.el
@@ -5,31 +5,31 @@
   (custom-theme-set-faces
    'custom-dark
    `(default ((,class (:foreground "#ffffff" :background "#000000"))))
-   `(cursor ((,class (:background "#ffffff"))))
+   `(cursor ((,class (:background "#e34234"))))
    `(fringe ((,class (:background "#000000"))))
-   `(mode-line ((,class (:foreground "#abb2bf" :background "#000000"))))
+   `(mode-line ((,class (:foreground "#000000" :background "#e34234"))))
    `(mode-line-inactive ((,class (:foreground "#5c6370" :background "#282c34"))))
    `(region ((,class (:background "#3e4451"))))
-   `(highlight ((,class (:background "#3e4451"))))
+   `(highlight ((,class (:background "#e34234"))))
    `(secondary-selection ((,class (:background "#565c64"))))
-   `(font-lock-builtin-face ((,class (:foreground "#61afef"))))
+   `(font-lock-builtin-face ((,class (:foreground "#e34234"))))
    `(font-lock-comment-delimiter-face ((,class (:foreground "#5c6370"))))
    `(font-lock-comment-face ((,class (:foreground "#5c6370"))))
    `(font-lock-constant-face ((,class (:foreground "#e5c07b"))))
    `(font-lock-doc-face ((,class (:foreground "#5c6370"))))
    `(font-lock-function-name-face ((,class (:foreground "#c678dd"))))
-   `(font-lock-keyword-face ((,class (:foreground "#61afef"))))
-   `(font-lock-negation-char-face ((,class (:foreground "#61afef"))))
-   `(font-lock-preprocessor-face ((,class (:foreground "#61afef"))))
-   `(font-lock-regexp-grouping-backslash ((,class (:foreground "#61afef"))))
+   `(font-lock-keyword-face ((,class (:foreground "#e34234"))))
+   `(font-lock-negation-char-face ((,class (:foreground "#e34234"))))
+   `(font-lock-preprocessor-face ((,class (:foreground "#e34234"))))
+   `(font-lock-regexp-grouping-backslash ((,class (:foreground "#e34234"))))
    `(font-lock-regexp-grouping-construct ((,class (:foreground "#e5c07b"))))
    `(font-lock-string-face ((,class (:foreground "#98c379"))))
    `(font-lock-type-face ((,class (:foreground "#56b6c2"))))
    `(font-lock-variable-name-face ((,class (:foreground "#e5c07b"))))
    `(font-lock-warning-face ((,class (:foreground "#ff6c6b"))))
-   `(button ((,class (:foreground "#61afef" :underline t))))
-   `(link ((,class (:foreground "#61afef" :underline t))))
-   `(link-visited ((,class (:foreground "#61afef" :underline t))))
+   `(button ((,class (:foreground "#e34234" :underline t))))
+   `(link ((,class (:foreground "#e34234" :underline t))))
+   `(link-visited ((,class (:foreground "#e34234" :underline t))))
    `(fringe ((,class (:background "#000000"))))
    `(header-line ((,class (:foreground "#abb2bf" :background "#282c34"))))
    `(tooltip ((,class (:foreground "#abb2bf" :background "#333842"))))))
@@ -37,7 +37,7 @@
 (custom-theme-set-variables
  'custom-dark
  '(ansi-color-names-vector
-   ["#000000" "#ff6c6b" "#98c379" "#e5c07b"
-    "#61afef" "#c678dd" "#56b6c2" "#abb2bf"]))
+   ["#000000" "#ff6c6b" "#98c379" "#e34234"
+    "#e34234" "#c678dd" "#56b6c2" "#abb2bf"]))
 
 (provide-theme 'custom-dark)
